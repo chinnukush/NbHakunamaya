ce849ae5ceaa55c5dd8b7b8f2c0bb3f961dd0870
chinnukush <chinnukushal123@gmail.com>
2026-03-01 19:04:26 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -31,13 +31,12 @@ DEFAULT_IMAGE_URL = "https://te.legra.ph/file/88d845b4f8a024a71465d.jpg"
 
 SILENTX_PREMIUM_UPDATE = """
 
-<b>🎬 ᴛɪᴛʟᴇ</b>: {}  <code>{}</code>
-<b>🎭 ᴅɪʀᴇᴄᴛᴏʀ</b>: {}
-<b>📅 ʀᴇʟᴇᴀꜱᴇ</b>: {}
+<b>🎬 ᴛɪᴛʟᴇ</b> : {}  <code>{}</code>
+<b>🔈 ʟᴀɴɢᴜᴀɢᴇ</b> : {}
 <b>⭐ ɪᴍᴅʙ</b>: {}/10 (<code>{}</code> votes)
 <b>🏷️ ɢᴇɴʀᴇꜱ</b>: {}
 <b>🔈 ᴀᴜᴅɪᴏ</b>: {}
-<b>📺 ғᴏʀᴍᴀᴛ</b>: {}
+<b>📺 ɢᴇɴʀᴇꜱ</b>: {}
 
 <b>⚡ 𝖕𝖔𝖜𝖊𝖗𝖉 𝖇𝖞 @Hari_Moviez</b>
 
